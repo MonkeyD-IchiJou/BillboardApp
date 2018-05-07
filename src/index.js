c2890dbf9fc9bdfebc4f6622380597a3198ba0a8
Ichi Jou <ichijou8282@gmail.com>
2018-05-07 12:18:32 +0800

    code optimised.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
